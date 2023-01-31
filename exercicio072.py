550a8b7296b8fbff823a908c1724505629434c10
juanmangueira <juanmangueiraa@gmail.com>
2023-01-31 08:26:10 -0300

    first commit

---
+++ exercicio072.py
@@ -0,0 +1,7 @@
+numeros_por_extenso = ( 'zero', 'um', 'dois', 
+            'três', 'quatro', 'cinco', 
+            'seis', 'sete', 'oito', 
+            'nove', 'dez', 'onze', 
+            'doze', 'treze', 'quartorze',
+            'quinze', 'dezesseis', 'dezessete',
+            'dezoito', 'dezenove', 'vinte')
