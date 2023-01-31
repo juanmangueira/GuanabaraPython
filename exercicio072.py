a849bf54a60b484c35ce498a1873dacafc8d1289
juanmangueira <juanmangueiraa@gmail.com>
2023-01-31 08:39:59 -0300

    removendo arquivo não validado

--- exercicio072.py
+++
@@ -1,7 +0,0 @@
-numeros_por_extenso = ( 'zero', 'um', 'dois', 
-            'três', 'quatro', 'cinco', 
-            'seis', 'sete', 'oito', 
-            'nove', 'dez', 'onze', 
-            'doze', 'treze', 'quartorze',
-            'quinze', 'dezesseis', 'dezessete',
-            'dezoito', 'dezenove', 'vinte')
